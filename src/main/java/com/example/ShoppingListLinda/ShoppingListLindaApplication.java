1f75e55fd62ed3912629ac7b21229b037a464168
Linda Siebert <linda.p.siebert@gmail.com>
2021-02-04 16:22:07 +0100

    This should create a war file. I created it by running the application as a Maven project in eclipse with the goal "clean install" per the tutorial: https://howtodoinjava.com/spring-boot2/war-packaging-example/

--- src/main/java/com/example/ShoppingListLinda/ShoppingListLindaApplication.java
+++ src/main/java/com/example/ShoppingListLinda/ShoppingListLindaApplication.java
@@ -2,9 +2,10 @@ package com.example.ShoppingListLinda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShoppingListLindaApplication {
+public class ShoppingListLindaApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingListLindaApplication.class, args);
